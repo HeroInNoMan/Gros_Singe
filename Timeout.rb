f29f0a7ca93e2bd780a5076949cefadfe452d4cb
Arthur Léothaud <duncan@tristramg.eu>
2010-10-11 10:36:53 +0200

    misc corrections

---
+++ Timeout.rb
@@ -0,0 +1,28 @@
+
+class Timeout
+  def initialize(tps, &blk)
+    @last = Time.now
+    @thread = Thread.new do
+      while true do
+        sleep(1)
+        if Time.now - @last > tps
+          blk.call
+          @last = Time.now
+        end
+      end
+    end
+    @thread.run
+  end
+
+  def reset
+    @last = Time.now
+  end
+
+  def stop
+    @thread.stop
+  end
+
+  def run
+    @thread.run
+  end
+end
